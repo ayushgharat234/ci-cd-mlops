b09684313d5a8a142d4df95738b5b35645569466
Ayush Gharat <ayushgharat234@gmail.com>
2025-07-28 10:11:08 +0000

    Model Serving Pipeline Integrated

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.10-slim
 WORKDIR /app
 COPY model/train.py ./train.py
-RUN pip install --no-cache-dir scikit-learn joblib 
-CMD ["python", "train.py"]
+COPY endpoints/serve.py ./serve.py
+RUN pip install --no-cache-dir scikit-learn joblib flask gunicorn
+RUN train.py 
+EXPOSE 8080
+CMD ["gunicorn", "--bind", "0.0.0.0:8080", "serve:app"]
---
+++ endpoints/serve.py
@@ -0,0 +1,17 @@
+import joblib
+from flask import Flask, request, jsonify
+import numpy as np
+
+app =  Flask(__name__)
+
+model = joblib.load("models/iris_model.joblib")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        data = request.get_json()
+        input_features = np.array(data['instances'])
+        predictions = model.predict(input_features).to_list()
+        return jsonify({'predictions': predictions})
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
